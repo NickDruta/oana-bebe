0bb87327b5b0cb45cd293597f45ef910b3e773d8
NickDruta <nickdruta@gmail.com>
2023-10-15 21:11:28 +0300

    Initialize project and build the pilons of arhitecture of the app

---
+++ src/app/App/ui/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import cls from './App.module.scss';
+
+const App = () => {
+  return (
+    <div className={cls.appWrapper}>
+      Oana Bebe
+    </div>
+  );
+}
+
+export default App;
